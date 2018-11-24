4238149936c0fc42568ad0601122e89c962baaf1
OgheneMichael <zonalmeet@gmail.com>
2018-11-24 16:26:54 +0100

    Scope chain in practice

--- 3-how-JS-works/script.js
+++ 3-how-JS-works/script.js
@@ -3,6 +3,8 @@
  */
 
 // functions
+
+/*
 calculateAge(1965);
 
 function calculateAge(year) {
@@ -26,3 +28,57 @@ function foo() {
 foo();
 
 console.log(age);
+
+*/
+
+/*****************************
+ * Lecture: Scoping
+ */
+
+
+// First scoping example
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+  var b = 'Hi!';
+  second();
+
+  function second() {
+    var c = 'Hey!';
+    console.log(a + b + c);
+  }
+}
+
+
+*/
+
+// Example to show the differece between execution stack and scope chain
+
+
+var a = 'Hello!';
+first();
+
+function first() {
+  var b = 'Hi!';
+  second();
+
+  function second() {
+    var c = 'Hey!';
+    third()
+  }
+}
+
+function third() {
+  var d = 'John';
+  // console.log(a + b + c + d);
+  console.log(a + d);
+}
+
+
+
+
+///////////////////////////////////////
+// Lecture: The this keyword
