6091b0fa9e3c9e4b3e565fbb2988f9ff30089a37
OgheneMichael <zonalmeet@gmail.com>
2018-11-26 23:01:15 +0100

    Class toggling

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -10,11 +10,11 @@ GAME RULES:
 */
 
 
-var scores, roundScores, activePlayer;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
-roundScores = 0;
-activePlayer = 1;
+roundScore = 0;
+activePlayer = 0;
 
 // console.log(dice);
 
@@ -41,6 +41,28 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 
   // 3. Update the round score IF the rolled number was NOT a 1
+  if (dice !== 1) {
+    //Add score
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    //Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
+
+
+    document.querySelector('.dice').style.display = 'none';
+
+  }
 
 });
 
