3d7007653dfcb06e29a5c8aa0f5164cf658937ef
OgheneMichael <zonalmeet@gmail.com>
2018-11-27 07:38:30 +0100

    DRY principle

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -47,24 +47,54 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     document.querySelector("#current-" + activePlayer).textContent = roundScore;
   } else {
     //Next player
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-
-    // document.querySelector('.player-0-panel').classList.remove('active');
-    // document.querySelector('.player-1-panel').classList.add('active');
-
-
-    document.querySelector('.dice').style.display = 'none';
+    nextPlayer();
 
   }
 
 });
+
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  // Add current score to global score
+  scores[activePlayer] += roundScore;
+
+  // Update the UI
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+  //Check if Player won the game
+  if (scores[activePlayer] >= 10) {
+    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
+  } else {
+    // Next Player
+    nextPlayer();
+  }
+
+
+
+
+})
+
+
+function nextPlayer() {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  // document.querySelector('.player-0-panel').classList.remove('active');
+  // document.querySelector('.player-1-panel').classList.add('active');
+
+
+  document.querySelector('.dice').style.display = 'none';
+}
 
 
 // document.querySelector("#current-" + activePlayer).textContent = dice;
