8e99addb59a132b31b7b29bd4e2bf8b93f449117
OgheneMichael <zonalmeet@gmail.com>
2018-11-27 23:38:49 +0100

    Primitives vs Objects

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -73,6 +73,47 @@ var jane = Object.create(personProto, {
 
 */
 
+// Primitives vs Objects
+
+
+// Primitives
+var a = 23;
+var b = a;
+a = 46;
+
+console.log(a, b);
+
+
+// Objects
+var obj1 = {
+  name: 'John',
+  age: 26
+}
+var obj2 = obj1;
+obj1.age = 30;
+
+console.log(obj1.age);
+console.log(obj2.age);
+
+
+// Functions
+var age = 27;
+var obj = {
+  name: 'Michael',
+  city: 'Lagos'
+};
+
+function change(a, b) {
+  a = 30;
+  b.city = 'New York';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
+
+
 
 
 
