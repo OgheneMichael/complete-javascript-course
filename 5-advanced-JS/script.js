1110e0fb7ef268d4cb8c7b2b1235848679685eb7
OgheneMichael <zonalmeet@gmail.com>
2018-11-28 07:16:34 +0100

    Functions returning functions

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -117,6 +117,7 @@ console.log(obj.city);
 
 ///////////////////////////////////////////
 // Lecture: Passing Functions as arguments
+/*
 
 var years = [1990, 1965, 1937, 2005, 1998];
 
@@ -154,6 +155,39 @@ var rates = arrayCalc(ages, maxHeartRate);
 
 console.log(ages);
 console.log(rates);
+
+*/
+
+///////////////////////////////////////////
+// Lecture: Functions returning functions
+
+function interviewQuestion(job) {
+  if (job === 'designer') {
+    return function(name) {
+      console.log(name + ', can you please explain what UX design is?');
+    };
+  } else if (job === 'teacher') {
+    return function(name) {
+      console.log('What subject do you teach, ' + name + '?');
+    };
+  } else {
+    return function(name) {
+      console.log('Hello ' + name + ', what do you do?');
+    };
+  }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+teacherQuestion('John');
+designerQuestion('John');
+designerQuestion('Jane');
+designerQuestion('Mark');
+designerQuestion('Mike');
+
+interviewQuestion('teacher')('Mark');
+
 
 
 ///
