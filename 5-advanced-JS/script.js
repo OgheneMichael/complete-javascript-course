878eba5bc3ada730ed916597edbf9ab4df4b9291
OgheneMichael <zonalmeet@gmail.com>
2018-11-28 07:57:43 +0100

    Closures

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -200,6 +200,7 @@ function game() {
 }
 game();
 */
+/*
 
 (
   function() {
@@ -217,5 +218,44 @@ game();
     console.log(score >= 5 - goodLuck);
   }
 )(5);
+*/
+
+
+///////////////////////////////////////////
+// Lecture: Closures
+/*
+function retirement(retirementAge) {
+  var a = ' years left until retirement';
+
+  return function(yearOfBirth) {
+    var age = 2016 - yearOfBirth;
+    console.log((retirementAge - age) + a);
+  };
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementGermany(1990);
+retirementUS(1990);
+retirementIceland(1990);
+retirement(66)(1990);
+*/
+
+function interviewQuestion(job) {
+  return function(name) {
+    if (job === 'designer') {
+      console.log(name + ', can you please explain what UX design is?');
+    } else if (job === 'teacher') {
+      console.log('What subject do you teach, ' + name + '?');
+    } else {
+      console.log('Hello ' + name + ', what do you do?');
+    }
+  };
+}
+interviewQuestion('teacher')('Mike');
+interviewQuestion('designer')('Jane');
+interviewQuestion()('Mark');
 
 ///
