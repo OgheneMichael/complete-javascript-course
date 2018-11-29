5c9bb3715509369bb5d919e37f5dcb19f4fa8983
OgheneMichael <zonalmeet@gmail.com>
2018-11-29 02:23:33 +0100

    Function initialization

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -36,13 +36,22 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var DOM = UICtrl.getDOMstrings();
+  var setUpEventListeners = function() {
+    var DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  }
+
 
   var ctrlAddItem = function() {
     // 1. Get filled input data
     var input = UICtrl.getInput();
-
-    console.log(input);
 
     // 2. Add the item to the budget controller
 
@@ -52,14 +61,16 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     // 5. Display the budget on the UI
 
-  }
+  };
 
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-  document.addEventListener('keypress', function(event) {
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
+  return {
+    init: function() {
+      console.log('Application has started!');
+      setUpEventListeners();
     }
-  });
+  };
 
 })(budgetController, UIController);
+
+
+controller.init();
