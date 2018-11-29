d0033c5191069bbf7df3ec03eddfd37eb7db308a
OgheneMichael <zonalmeet@gmail.com>
2018-11-29 13:41:02 +0100

    Updating budget controller

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -13,7 +13,13 @@ var budgetController = (function() {
     this.value = value;
   };
 
-
+  var calculateTotal = function(type) {
+    var sum = 0;
+    data.allItems[type].forEach(function(cur) {
+      sum += cur.value;
+    });
+    data.totals[type] = sum;
+  };
 
   var data = {
     allItems: {
@@ -23,7 +29,9 @@ var budgetController = (function() {
     totals: {
       exp: 0,
       inc: 0
-    }
+    },
+    budget: 0,
+    percentage: -1
   };
 
   return {
@@ -50,6 +58,33 @@ var budgetController = (function() {
 
       // Return the new Element
       return newItem;
+    },
+
+    calculateBudget: function() {
+
+      // 1. Calculate total income and expenses
+      calculateTotal('exp');
+      calculateTotal('inc');
+
+      // 2. Calculate the budget: income - expenses
+      data.budget = data.totals.inc - data.totals.exp;
+
+      // 3. Calculate the percentage of income that we spent
+      if (data.totals.inc > 0) {
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+      } else {
+        data.percentage = -1;
+      }
+
+    },
+
+    getBudget: function() {
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+        percentage: data.percentage
+      };
     },
 
     testing: function() {
@@ -145,10 +180,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   var updateBudget = function() {
     // 1. Calculate the budget
+    budgetCtrl.calculateBudget();
 
     // 2. Return the budget
+    var budget = budgetCtrl.getBudget();
 
     // 3. Display the budget on the UI
+    console.log(budget);
   };
 
 
