359f9d09ba8c940f448aa512c14b8e8fb0e1cc6b
OgheneMichael <zonalmeet@gmail.com>
2018-11-29 01:14:04 +0100

    Setup keypress event listener

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -1,35 +1,42 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
-
-  var x = 23;
-
-  var add = function(a) {
-    return x + a;
-  }
-
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  };
-
-})();
-
-
-var UIController = (function() {
 
   // Some code
 
 })();
 
 
+
+// UI CONTROLLER
+var UIController = (function() {
+
+  // Some code
+
+})();
+
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var z = budgetCtrl.publicTest(5);
+  var ctrlAddItem = function() {
+    // 1. Get filled input data
 
-  return {
-    anotherPublic: function() {
-      console.log(z);
+    // 2. Add the item to the budget controller
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the UI
+
+    console.log('It works');
+  }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
     }
-  };
+  });
 
 })(budgetController, UIController);
