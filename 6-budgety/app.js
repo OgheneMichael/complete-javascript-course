1b126cb4c8371a3980738f222e615235e3dcaeff
OgheneMichael <zonalmeet@gmail.com>
2018-11-30 23:03:22 +0100

    Finished the project with event change

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -180,6 +180,12 @@ var UIController = (function() {
 
   };
 
+  var nodeListForEach = function(list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i);
+    }
+  };
+
   return {
     getInput: function() {
       return {
@@ -254,12 +260,6 @@ var UIController = (function() {
 
       var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-      var nodeListForEach = function(list, callback) {
-        for (var i = 0; i < list.length; i++) {
-          callback(list[i], i);
-        }
-      };
-
       nodeListForEach(fields, function(current, index) {
 
         if (percentages[index] > 0) {
@@ -297,6 +297,22 @@ var UIController = (function() {
 
     },
 
+    changedType: function() {
+
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType + ',' +
+        DOMstrings.inputDescription + ',' +
+        DOMstrings.inputValue
+      );
+
+      nodeListForEach(fields, function(cur) {
+        cur.classList.toggle('red-focus');
+      });
+
+      document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -319,7 +335,10 @@ var controller = (function(budgetCtrl, UICtrl) {
     });
 
     document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
-  }
+
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
+
+  };
 
 
   var updateBudget = function() {
