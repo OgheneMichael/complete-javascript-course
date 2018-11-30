7df0a85070913fd1ea94d3c99d6734933b4e46e8
OgheneMichael <zonalmeet@gmail.com>
2018-11-30 20:26:06 +0100

    Biolerplate for calculation and updating of percentages

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -229,6 +229,15 @@ var controller = (function(budgetCtrl, UICtrl) {
     UICtrl.displayBudget(budget);
   };
 
+  var updatePercentages = function() {
+
+    // 1. Calculate the percentages
+
+    // 2. Read percentages from the budget controller
+
+    // 3. Update the UI with the new percentages
+
+  };
 
   var ctrlAddItem = function() {
     var input, newItem;
@@ -248,6 +257,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 
       // 5. Calculate and update budget
       updateBudget();
+
+      // 6. Calculate and update percentages
+      updatePercentages();
     }
 
   };
@@ -271,6 +283,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 
       // 3. Update and show the new budget
       updateBudget();
+
+      // 4. Calculate and update percentages
+      updatePercentages();
     }
 
   }
