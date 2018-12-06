0d7b170d6e5a9d971a0f808e2f98db738983fcc8
OgheneMichael <zonalmeet@gmail.com>
2018-12-06 06:50:48 +0100

    Arrow functions

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -84,6 +84,7 @@ console.log(c);
 //////////////////////////////
 // Lecture: Strings
 
+/*
 let firstName = 'John';
 let lastName = 'Smith';
 const yearOfBirth = 1990;
@@ -103,3 +104,30 @@ console.log(n.startsWith('j'));
 console.log(n.endsWith('sm'));
 console.log(n.includes('oh'));
 console.log(`${firstName} `.repeat(5));
+*/
+
+////////////////////////////////
+// Lecture: Arrow functions
+
+const years = [1990, 1965, 1982, 1937];
+
+// ES5
+var ages5 = years.map(function(el) {
+  return 2016 - el;
+});
+console.log(ages5);
+
+
+// ES6
+let ages6 = years.map(el => 2016 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016 - el}.`);
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+  const now = new Date().getFullYear();
+  const age = now - el;
+  return `Age element ${index + 1}: ${age}.`;
+});
+console.log(ages6);
