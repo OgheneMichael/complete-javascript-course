f7b4d83bd5e15233015830e3fe2f99903efc31f1
OgheneMichael <zonalmeet@gmail.com>
2018-12-06 06:06:59 +0100

    Blocks and IIFEs

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -15,6 +15,7 @@ console.log(name6);
 
 */
 
+/*
 // ES5 ––– Function scoped
 function driversLicense5(passedTest) {
 
@@ -54,3 +55,26 @@ for (var i = 0; i < 5; i++) {
 }
 
 console.log(i);
+*/
+
+
+//////////////////////////////
+// Lecture: Blocks and IIFEs
+
+// ES6;
+{
+  const a = 1;
+  let b = 2;
+  var c = 3;
+}
+
+// console.log(a + b);
+console.log(c);
+
+// ES5
+(function() {
+
+  var c = 3;
+})();
+
+// console.log(c);
