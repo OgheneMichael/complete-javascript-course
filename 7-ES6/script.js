ce4063e65865e3bbbbf93e0c507c311a6a36b2cc
OgheneMichael <zonalmeet@gmail.com>
2018-12-08 15:00:10 +0100

    for...of loop

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -261,12 +261,32 @@ console.log(retirement);
 const boxes = document.querySelectorAll('.box');
 
 // ES5
-/*
+
 var boxesArr5 = Array.prototype.slice.call(boxes);
 boxesArr5.forEach(function(cur) {
   cur.style.backgroundColor = 'dodgerblue';
 });
-*/
+
 
 // ES6
-Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+const boxesArr6 = Array.from(boxes);
+boxesArr6.forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+
+
+// ES5
+/*
+for (var i = 0; i < boxesArr5.length; i++) {
+  if (boxesArr5[i].className === 'box blue') {
+    // continue;
+    break
+  }
+  boxesArr5[i].textContent = 'I changed to blue!';
+}
+*/
+
+for (const cur of boxesArr6) {
+  if (cur.className.includes('blue')) {
+    continue;
+  }
+  cur.textContent = 'I changed to blue!';
+}
