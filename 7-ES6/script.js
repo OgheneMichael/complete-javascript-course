4768085c24b32cc3d84753ac6642c6d38a833988
OgheneMichael <zonalmeet@gmail.com>
2018-12-08 15:24:08 +0100

    Spread operator – joining arrays

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -327,3 +327,9 @@ console.log(sum2);
 // ES6
 const sum3 = addFourAges(...ages);
 console.log(sum3);
+
+const familySmith = ['John', 'Jane', 'Mark'];
+const familyMiller = ['Mary', 'Bob', 'Ann'];
+
+const bigFamily = [...familySmith, 'Lily', ...familyMiller];
+console.log(bigFamily);
