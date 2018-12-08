6239d66f80e73cf51de764b37500b818c8359bc5
OgheneMichael <zonalmeet@gmail.com>
2018-12-08 20:32:46 +0100

    ES6 Maps

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -435,13 +435,24 @@ question.set(true, 'Correct answer :D');
 question.set(false, 'Wrong, please try again');
 
 console.log(question.get('question'));
-console.log(question.size);
+// console.log(question.size);
 
 if (question.has(4)) {
   // question.delete(4);
-  console.log('Answer 4 is here');
+  // console.log('Answer 4 is here');
 }
 
 // question.clear();
+
+// question.forEach((value, key) => console.log(`This is ${key}, and it's set to ${value}.`));
+
+for (let [key, value] of question.entries()) {
+  if (typeof(key) === 'number') {
+    console.log(`Answer ${key}: ${value}`)
+  }
+}
+
+const ans = parseInt(prompt('Write the correct answer'));
+console.log(question.get(ans === question.get('correct')));
 
 //
