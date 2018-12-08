597db6f9956477dc1a9900a67e99942ff625cefd
OgheneMichael <zonalmeet@gmail.com>
2018-12-08 20:48:43 +0100

    ES6 Classes

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -424,6 +424,7 @@ var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish');
 ////////////////////////////////
 // Lecture: Maps
 
+/*
 const question = new Map();
 question.set('question', 'What is the official name of the latest major JavaScript version?');
 question.set(1, 'ES5');
@@ -454,5 +455,51 @@ for (let [key, value] of question.entries()) {
 
 const ans = parseInt(prompt('Write the correct answer'));
 console.log(question.get(ans === question.get('correct')));
+*/
+
+
+
+
+////////////////////////////////
+// Lecture: Classes
+
+// ES5
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+}
+
+Person5.prototype.calculateAge = function() {
+  var age = new Date().getFullYear() - this.yearOfBirth;
+  console.log(age);
+}
+
+var john5 = new Person5('John', 1990, 'Teacher');
+
+
+// ES6
+class Person6 {
+  constructor(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+  }
+
+  calculateAge() {
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+  }
+
+  static greeting() {
+    console.log('Hey there!');
+  }
+
+}
+
+const john6 = new Person6('John', 1990, 'Teacher');
+
+Person6.greeting();
+
 
 //
