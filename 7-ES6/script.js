e23fccb501bc1f91d13b5f2b10a32fbee5996470
OgheneMichael <zonalmeet@gmail.com>
2018-12-08 13:50:30 +0100

    ES5 'this' hack

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -108,7 +108,7 @@ console.log(`${firstName} `.repeat(5));
 
 ////////////////////////////////
 // Lecture: Arrow functions
-
+/*
 const years = [1990, 1965, 1982, 1937];
 
 // ES5
@@ -131,3 +131,21 @@ ages6 = years.map((el, index) => {
   return `Age element ${index + 1}: ${age}.`;
 });
 console.log(ages6);
+*/
+
+////////////////////////////////
+// Lecture: Arrow functions 2
+
+// ES5
+var box5 = {
+  color: 'green',
+  position: 1,
+  clickMe: function() {
+    var self = this;
+    document.querySelector('.green').addEventListener('click', function() {
+      var str = 'This is box number ' + self.position + ' and it is ' + self.color;
+      alert(str);
+    })
+  }
+}
+box5.clickMe();
