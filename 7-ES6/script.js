6cfdf79014187d29c84001370e01e46d4b89faed
OgheneMichael <zonalmeet@gmail.com>
2018-12-08 15:28:53 +0100

    Spread operator – joining nodes

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -333,3 +333,9 @@ const familyMiller = ['Mary', 'Bob', 'Ann'];
 
 const bigFamily = [...familySmith, 'Lily', ...familyMiller];
 console.log(bigFamily);
+
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+const all = [h, ...boxes];
+
+Array.from(all).forEach(cur => cur.style.color = 'purple');
