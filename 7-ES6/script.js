a3c29c51dd3156b9b8520770f97a97b5d13b1ff4
OgheneMichael <zonalmeet@gmail.com>
2018-12-08 14:13:21 +0100

    Arrow functions Lexical 'this' Keyword

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -148,4 +148,61 @@ var box5 = {
     })
   }
 }
-box5.clickMe();
+// box5.clickMe();
+
+
+// ES6
+const box6 = {
+  color: 'green',
+  position: 1,
+  clickMe: function() {
+    document.querySelector('.green').addEventListener('click', () => {
+      var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+      alert(str);
+    })
+  }
+}
+box6.clickMe();
+
+/*
+const box66 = {
+  color: 'green',
+  position: 1,
+  clickMe: () => {
+    document.querySelector('.green').addEventListener('click', () => {
+      var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+      alert(str);
+    })
+  }
+}
+box66.clickMe();
+*/
+
+
+
+function Person(name) {
+  this.name = name;
+}
+
+//ES5
+Person.prototype.myFriends5 = function(friends) {
+  var arr = friends.map(function(el) {
+    return this.name + ' is friends with ' + el;
+  }.bind(this));
+
+  console.log(arr);
+};
+var friends = ['Bob', 'Jane', 'Mark'];
+
+new Person('John').myFriends5(friends);
+
+
+//ES6
+Person.prototype.myFriends6 = function(friends) {
+  var arr = friends.map(el => `${this.name} is friends with ${el}`);
+
+  console.log(arr);
+};
+// var friends = ['Bob', 'Jane', 'Mark'];
+
+new Person('Michael').myFriends6(friends);
