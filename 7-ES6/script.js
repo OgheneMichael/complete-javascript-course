b8b99e469708ba86e2cff7e4e686ad6435f1e27d
OgheneMichael <zonalmeet@gmail.com>
2018-12-08 21:20:54 +0100

    Classes and subclasses

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -463,6 +463,7 @@ console.log(question.get(ans === question.get('correct')));
 ////////////////////////////////
 // Lecture: Classes
 
+/*
 // ES5
 var Person5 = function(name, yearOfBirth, job) {
   this.name = name;
@@ -500,6 +501,78 @@ class Person6 {
 const john6 = new Person6('John', 1990, 'Teacher');
 
 Person6.greeting();
+*/
 
 
-//
+
+////////////////////////////////
+// Lecture: Classes and subclasses
+
+
+// ES5
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+}
+
+Person5.prototype.calculateAge = function() {
+  var age = new Date().getFullYear() - this.yearOfBirth;
+  console.log(age);
+}
+
+var Athlete5 = function(name, yearOfBirth, job, olympicGames, medals) {
+  Person5.call(this, name, yearOfBirth, job);
+
+  this.olympicGames = olympicGames;
+  this.medals = medals;
+}
+
+
+
+Athlete5.prototype = Object.create(Person5.prototype);
+
+Athlete5.prototype.wonMedal = function() {
+  this.medals++;
+  console.log(this.medals);
+};
+
+var johnAthlete5 = new Athlete5('John', 1990, 'swimmer', 3, 10);
+
+johnAthlete5.calculateAge();
+johnAthlete5.wonMedal();
+
+
+
+
+// ES6
+class Person6 {
+  constructor(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+  }
+
+  calculateAge() {
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+  }
+}
+
+class Athlete6 extends Person6 {
+  constructor(name, yearOfBirth, job, olympicGames, medals) {
+    super(name, yearOfBirth, job);
+    this.olympicGames = olympicGames;
+    this.medals = medals;
+  }
+
+  wonMedal() {
+    this.medals++;
+    console.log(this.medals);
+  }
+}
+
+const johnAthlete6 = new Athlete6('John', 1990, 'swimmer', 3, 10);
+johnAthlete6.wonMedal();
+johnAthlete6.calculateAge();
+////
