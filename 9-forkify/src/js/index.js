a4d2e903db0dcce6ce2031ea0c9b55900d9b09a8
OgheneMichael <zonalmeet@gmail.com>
2018-12-12 02:07:20 +0100

    error handling with axios

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -3,11 +3,16 @@ import axios from 'axios';
 async function getResults(query) {
   const proxy = 'https://cors-anywhere.herokuapp.com/'
   const key = '64f5793f1f4299767bf25a86015933f0';
-  const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-  const recipes = res.data.recipes;
-  console.log(recipes);
+  try {
+    const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch (e) {
+    alert(e)
+  }
+
 }
 
-getResults('pizza');
+getResults('pasta');
 
 //https://www.food2fork.com/api/search
