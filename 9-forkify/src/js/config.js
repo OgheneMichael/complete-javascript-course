38c4b6ff19a79b0f68bfa4916647a0832061b00a
OgheneMichael <zonalmeet@gmail.com>
2018-12-19 23:20:00 +0100

    Created config file for API key and Proxy

---
+++ 9-forkify/src/js/config.js
@@ -0,0 +1,2 @@
+export const proxy = "https://cors-anywhere.herokuapp.com/";
+export const key = "64f5793f1f4299767bf25a86015933f0";
--- 9-forkify/src/js/models/Search.js
+++ 9-forkify/src/js/models/Search.js
@@ -1,19 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
+import { key, proxy } from "../config";
 
 export default class Search {
-  constructor(query) {
-    this.query = query;
-  }
+	constructor(query) {
+		this.query = query;
+	}
 
-  async getResults() {
-    const proxy = 'https://cors-anywhere.herokuapp.com/'
-    const key = '64f5793f1f4299767bf25a86015933f0';
-    try {
-      const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
-      this.result = res.data.recipes;
-      // console.log(this.result);
-    } catch (e) {
-      alert(e)
-    }
-  }
+	async getResults() {
+		try {
+			const res = await axios(
+				`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${
+					this.query
+				}`
+			);
+			this.result = res.data.recipes;
+			// console.log(this.result);
+		} catch (e) {
+			alert(e);
+		}
+	}
 }
