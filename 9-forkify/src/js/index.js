c3649c226d9205921dc5894dc051d10010f8e62a
OgheneMichael <zonalmeet@gmail.com>
2018-12-19 22:42:54 +0100

    Finished building the search feature

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,8 +1,6 @@
-import Search from './models/Search';
-import * as searchView from './views/searchView';
-import {
-  elements
-} from './views/base';
+import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import { elements, renderLoader, clearLoader } from "./views/base";
 
 /**
  * - Search object
@@ -11,31 +9,42 @@ import {
  * - liked recipes
  */
 
-const state = {}
+const state = {};
 
 const controlSearch = async () => {
-  //1) Get query from the view
+	//1) Get query from the view
 
-  const query = searchView.getInput();
+	const query = searchView.getInput();
 
-  if (query) {
-    // 2) New search object and add to state
-    state.search = new Search(query);
+	if (query) {
+		// 2) New search object and add to state
+		state.search = new Search(query);
 
-    // 3) Prepare UI for result
-    searchView.clearInput();
-    searchView.clearResults();
+		// 3) Prepare UI for result
+		searchView.clearInput();
+		searchView.clearResults();
+		renderLoader(elements.searchRes);
 
-    // 4) Search for recipes
-    await state.search.getResults();
+		// 4) Search for recipes
+		await state.search.getResults();
 
-    // 5) Render results on UI
-    searchView.renderResult(state.search.result)
-    // console.log(state.search.result);
-  }
-}
+		// 5) Render results on UI
+		clearLoader();
+		searchView.renderResult(state.search.result);
+		// console.log(state.search.result);
+	}
+};
 
-elements.searchForm.addEventListener('submit', e => {
-  e.preventDefault();
-  controlSearch();
+elements.searchForm.addEventListener("submit", e => {
+	e.preventDefault();
+	controlSearch();
+});
+
+elements.searchResPages.addEventListener("click", e => {
+	const btn = e.target.closest(".btn-inline");
+	if (btn) {
+		const goToPage = parseInt(btn.dataset.goto, 10);
+		searchView.clearResults();
+		searchView.renderResult(state.search.result, goToPage);
+	}
 });
--- 9-forkify/src/js/views/searchView.js
+++ 9-forkify/src/js/views/searchView.js
@@ -1,16 +1,15 @@
-import {
-  elements
-} from './base';
+import { elements } from "./base";
 
 export const getInput = () => elements.searchInput.value;
 
 export const clearInput = () => {
-  elements.searchInput.value = '';
+	elements.searchInput.value = "";
 };
 
 export const clearResults = () => {
-  elements.searchResList.innerHTML = '';
-}
+	elements.searchResList.innerHTML = "";
+	elements.searchResPages.innerHTML = "";
+};
 /*
 // 'Pasta with tomato and spinach'
 acc: 0 / acc + cur.length = 5 / newTitle = ['Pasta']
@@ -20,24 +19,23 @@ acc: 15 / acc + cur.length = 18 / newTitle = ['Pasta', 'with' 'tomato']
 acc: 18 / acc + cur.length = 24 / newTitle = ['Pasta', 'with' 'tomato']
 */
 const limitRecipeTitle = (title, limit = 17) => {
-  const newTitle = [];
-  if (title.length > limit) {
-    title.split(' ').reduce((acc, cur) => {
-      if (acc + cur.length <= limit) {
-        newTitle.push(cur);
-      }
-      return acc + cur.length;
-    }, 0);
+	const newTitle = [];
+	if (title.length > limit) {
+		title.split(" ").reduce((acc, cur) => {
+			if (acc + cur.length <= limit) {
+				newTitle.push(cur);
+			}
+			return acc + cur.length;
+		}, 0);
 
-    //return the result
-    return `${newTitle.join(' ')} ...`;
-
-  }
-  return title;
-}
+		//return the result
+		return `${newTitle.join(" ")} ...`;
+	}
+	return title;
+};
 
 const renderRecipe = recipe => {
-  const markup = `
+	const markup = `
     <li>
         <a class="results__link" href="#${recipe.recipe_id}">
             <figure class="results__fig">
@@ -51,9 +49,51 @@ const renderRecipe = recipe => {
     </li>
   `;
 
-  elements.searchResList.insertAdjacentHTML('beforeend', markup);
+	elements.searchResList.insertAdjacentHTML("beforeend", markup);
 };
 
-export const renderResult = recipes => {
-  recipes.forEach(renderRecipe);
+// type: 'prev' or 'next'
+const createButton = (page, type) => `
+	<button class="btn-inline results__btn--${type}" data-goto='${
+	type === "prev" ? page - 1 : page + 1
+}'>
+		<span>Page ${type === "prev" ? page - 1 : page + 1}</span>
+		<svg class="search__icon">
+			<use href="img/icons.svg#icon-triangle-${
+				type === "prev" ? "left" : "right"
+			}"></use>
+		</svg>
+	</button>
+`;
+
+const renderButtons = (page, numResults, resPerPage) => {
+	const pages = Math.ceil(numResults / resPerPage);
+
+	let button;
+	if (page === 1 && pages > 1) {
+		// Only button to go to next page
+		button = createButton(page, "next");
+	} else if (page < pages) {
+		// Both buttons
+		button = `
+			${createButton(page, "prev")}
+			${createButton(page, "next")}
+		`;
+	} else if (page === pages) {
+		// Only button to go to prev page
+		button = createButton(page, "prev");
+	}
+
+	elements.searchResPages.insertAdjacentHTML("afterbegin", button);
+};
+
+export const renderResult = (recipes, page = 1, resPerPage = 10) => {
+	// render results of current page;
+	const start = (page - 1) * resPerPage;
+	const end = page * resPerPage;
+
+	recipes.slice(start, end).forEach(renderRecipe);
+
+	// render pagination buttons
+	renderButtons(page, recipes.length, resPerPage);
 };
