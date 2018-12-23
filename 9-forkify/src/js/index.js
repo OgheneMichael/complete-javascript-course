beea989ecd9e4a1678f5042a0dd20a1f69ca21f1
OgheneMichael <zonalmeet@gmail.com>
2018-12-23 05:30:15 +0100

    Implemented localStorage

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -140,9 +140,6 @@ elements.shopping.addEventListener("click", e => {
 /**
  * LIKE CONTROLLER
  */
-// TESTING
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
 
 const controlLike = () => {
 	if (!state.likes) state.likes = new Likes();
@@ -177,6 +174,21 @@ const controlLike = () => {
 	}
 	likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
+
+// Restore liked recipes on page load
+
+window.addEventListener("load", () => {
+	state.likes = new Likes();
+
+	// Restore likes
+	state.likes.readStorage();
+
+	// Toggle like menu button
+	likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+	// Render the existing likes
+	state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
 // Handling recipe button clicks
 elements.recipe.addEventListener("click", e => {
