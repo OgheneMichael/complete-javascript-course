35b01d64f7ee72ce1d8f33c8d90a250ba6133cf3
OgheneMichael <zonalmeet@gmail.com>
2018-12-23 04:22:24 +0100

    Built the Like Controller

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,6 +1,7 @@
 import Search from "./models/Search";
 import Recipe from "./models/Recipe";
 import List from "./models/List";
+import Likes from "./models/Likes";
 import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
 import * as listView from "./views/listView";
@@ -135,6 +136,39 @@ elements.shopping.addEventListener("click", e => {
 	}
 });
 
+/**
+ * LIKE CONTROLLER
+ */
+const controlLike = () => {
+	if (!state.likes) state.likes = new Likes();
+	const currentID = state.recipe.id;
+
+	// User has NOT yet liked current recipe
+	if (!state.likes.isLiked(currentID)) {
+		// Add like to the state
+		const newLike = state.likes.addLike(
+			currentID,
+			state.recipe.title,
+			state.recipe.author,
+			state.recipe.img
+		);
+
+		// Toggle the like button
+
+		// Add like to  UI list
+		console.log(state.likes);
+
+		// User HAS liked current recipe
+	} else {
+		// Remove like from the state
+		state.likes.deleteLike(currentID);
+
+		// Toggle the like button
+		// Remove like from  UI list
+		console.log(state.likes);
+	}
+};
+
 // Handling recipe button clicks
 elements.recipe.addEventListener("click", e => {
 	if (e.target.matches(".btn-decrease, .btn-decrease *")) {
@@ -146,9 +180,14 @@ elements.recipe.addEventListener("click", e => {
 	} else if (e.target.matches(".btn-increase, .btn-increase *")) {
 		// Increase button is clicked
 		state.recipe.updateServings("inc");
+
 		recipeView.updateServingsIngredients(state.recipe);
 	} else if (e.target.matches(".recipe__btn--add, .recipe__btn--add *")) {
+		// Add ingredients to shopping list
 		controlList();
+	} else if (e.target.matches(".recipe__love, .recipe__love *")) {
+		// Like controller
+		controlLike();
 	}
 });
 
